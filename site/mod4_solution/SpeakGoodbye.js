1917be91274b47a53be1f6b15186c4999dd58bf8
Thiago Furtado <thiagorbdfurtado@gmail.com>
2021-07-03 19:01:52 -0300

    Assignments

---
+++ site/mod4_solution/SpeakGoodbye.js
@@ -0,0 +1,9 @@
+(function(window){
+  var ByeSpeaker={};
+  var speakWord = "Good Bye";
+  ByeSpeaker.speak = function(name) {
+    console.log(speakWord + " " + name);
+  }
+
+  window.ByeSpeaker=ByeSpeaker;
+})(window);
---
+++ site/mod4_solution/script.js
@@ -0,0 +1,13 @@
+(function(window){
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  for(var i= 0; i < names.length; i++) {
+    var firstLetter;
+    firstLetter=names[i].charAt(0);
+    if (firstLetter=='J' || firstLetter=='j') {
+      ByeSpeaker.speak(names[i]);
+    } else {
+      HelloSpeaker.speak(names[i]);
+    }
+  }
+  window.names= names;
+})(window); 
