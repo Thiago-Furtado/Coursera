cbda05d1a00c3f149b50c89a0539f98871193b41
Thiago Furtado <thiagorbdfurtado@gmail.com>
2021-07-03 19:08:18 -0300

    Mod2

--- site/mod4_solution/SpeakGoodbye.js
+++ site/mod4_solution/SpeakGoodbye.js
@@ -1,8 +1,8 @@
 (function(window){
-  var ByeSpeaker={};
+  var byeSpeaker={};
   var speakWord = "Good Bye";
-  ByeSpeaker.speak = function(name) {
+  byeSpeaker.speak = function(name) {
     console.log(speakWord + " " + name);
   }
-  window.ByeSpeaker=ByeSpeaker;
+  window.byeSpeaker=byeSpeaker;
 })(window);
