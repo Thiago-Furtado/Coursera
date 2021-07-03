c22f420ddee575983ebcb5f205c3a3a02c480106
Thiago-Furtado <85516946+Thiago-Furtado@users.noreply.github.com>
2021-07-03 19:38:10 -0300

    Delete site/mod4_solution directory

--- site/mod4_solution/SpeakHello.js
+++
@@ -1,9 +0,0 @@
-(function(window){
-  var HelloSpeaker={};
-  var speakWord = "Hello";
-  HelloSpeaker.speak = function(name) {
-    console.log(speakWord + " " + name);
-  };
-
-  window.HelloSpeaker=HelloSpeaker;
-})(window);
